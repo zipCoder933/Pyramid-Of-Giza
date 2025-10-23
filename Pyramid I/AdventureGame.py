642ed2400cf9119e63ea0656ba3364f235f3369b
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-10-23 15:17:37 -0600

    fix

--- Pyramid I/AdventureGame.py
+++ Pyramid I/AdventureGame.py
@@ -1,11 +1,10 @@
 import time
 import os
-
-import os 
+import story as st 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 while(True):
-    os.system(f"python \"{dir_path}/adventure_game.py\"")
+    st.startGame()
     while(True):
         val = input("\n\nDo you want to play again? (Y/n): ").lower().strip()
         if(val == 'y' or val == 'yes'):
